a67f3b4e6d99130414f0dcfb32192f3232936e01
Mike <michaelokolo62@yahoo.com>
2025-06-14 00:05:05 +0100

    Set up get current user route

--- backend/src/controllers/userController/getCurrentUser.ts
+++ backend/src/controllers/userController/getCurrentUser.ts
@@ -1,10 +1,35 @@
-import { Request, Response } from 'express';
+import { Response, NextFunction } from 'express';
 import { AuthenticatedRequest } from '../../middleware/auth/authenticate';
+import prisma from '../../utils/db/prisma';
+import { UnauthorizedError } from '../../utils/errors/UnauthorizedError';
+import { NotFoundError } from '../../utils/errors/NotFoundError';
 
-export function getCurrentUser(req: AuthenticatedRequest, res: Response) {
-  if (req.user) {
-    console.log(`Current user ID: ${req.user.id}`);
+export async function getCurrentUser(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  if (!req.user) {
+    throw new UnauthorizedError('User not authenticated');
   }
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id: req.user?.id },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        role: true,
+      },
+    });
 
-  res.status(200).json({ message: 'I am the current user' });
+    if (!user) {
+      throw new NotFoundError('User not found');
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 }
